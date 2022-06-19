b2d3a108237a1b8e143158d72cbb07405e41b077
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-06-20 02:08:28 +0900

    Feat: axios default 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,42 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { useState } from "react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import axios from "axios";
+import cookies from "js-cookie";
+import { AuthService } from "../src/services";
+
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_HOST;
+axios.interceptors.request.use(
+  function (config) {
+    const accessToken = cookies.get("accessToken");
+    if (accessToken && config.headers) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  function (error) {
+    // 요청시 에러라함은 어떤게 있을까?
+    return Promise.reject(error);
+  }
+);
+axios.interceptors.response.use(
+  function (response) {
+    // 200 성공
+    return response;
+  },
+  function (error) {
+    // 200 이외의 실패
+    if (error.status === 401) {
+      const originalRequest = error.config;
+      AuthService.refresh();
+
+      // 요렇게 하면 interceptors.request 안으로 다시 들어가겠지?
+      return axios(originalRequest);
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
